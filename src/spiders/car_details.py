51360759de960f3daf376d8283e5edf58bb03fc3
ivanshmuliak <shmi03@vse.cz>
2025-02-06 17:28:42 +0100

    Ve třídě car_detail.py byly dodělány všechny atributy a následně celý proces byl rozdělen do dvou fází: extrakce a čištění.

--- src/spiders/car_details.py
+++ src/spiders/car_details.py
@@ -1,7 +1,6 @@
 import scrapy
 import csv
 import re
-from scrapy import Selector
 
 class CarDetailsSpider(scrapy.Spider):
     name = "car_details"
@@ -16,29 +15,43 @@ class CarDetailsSpider(scrapy.Spider):
 
     def parse(self, response):
         # Extrakce dat
-        car_model = re.sub(r'\s+', ' ', response.css("div.detail-hero__info__content__section > section > h1::text").get()).strip()
-        model_version = re.sub(r'\s+', ' ', response.css("div.detail-hero__info__content__section > section > p.text-M::text").get()).strip()
-        price = response.css("div.detail-hero__info__content__info > p.text-h1.highlighted::text").get()
-        registration_date = ' '.join(response.css('div.detail-hero__info__boxes > div.detail-hero__info__box:nth-of-type(1)::text').getall()).strip().replace(' ', '')
-        mileage = ' '.join(response.css('div.detail-hero__info__boxes > div.detail-hero__info__box:nth-of-type(2)::text').getall()).strip().replace(' ', '')
-        stk = re.sub(r"\s+", " ", " ".join(response.xpath("//div[@class='detail-box-M']/div/div[3]/text()").getall()).strip())
-        #stav
-        vykon = re.sub(r"\s+", " ", " ".join(response.xpath('//div[@class="detail-info__col"][1]/div[2]/div[1]/div[1]/text()').getall()).strip())
-        objem_motoru = re.sub(r"\s+", " ", " ".join(response.xpath('//div[@class="detail-info__col"][1]/div[2]/div[1]/div[2]/text()').getall()).strip())
-        typ_paliva = re.sub(r"\s+", " ", " ".join(response.xpath('//div[@class="detail-info__col"][1]/div[2]/div[1]/div[3]/text()').getall()).strip())
-        typ_prevodovky = re.sub(r"\s+", " ", " ".join(response.xpath('//div[@class="detail-info__col"][1]/div[2]/div[1]/div[4]/text()').getall()).strip())
-        typ_karoserie = re.sub(r"\s+", " ", " ".join(response.xpath('//div[@class="detail-info__col"][2]/div[2]/div[1]/div[1]/text()').getall()).strip())
-        pocet_dveri = re.sub(r"\s+", " ", " ".join(response.xpath('//div[@class="detail-info__col"][2]/div[2]/div[1]/div[2]/text()').getall()).strip())
-        pocet_mist = re.sub(r"\s+", " ", " ".join(response.xpath('//div[@class="detail-info__col"][2]/div[2]/div[1]/div[3]/text()').getall()).strip())
-        barva_exterieru = re.sub(r"\s+", " ", " ".join(response.xpath('//div[@class="detail-info__col"][2]/div[2]/div[1]/div[4]/text()').getall()).strip())
-        #pohon
-        #
+        model_auta = response.css("div.detail-hero__info__content__section > section > h1::text").get()
+        verze_modelu = response.css("div.detail-hero__info__content__section > section > p.text-M::text").get()
+        cena = response.css("div.detail-hero__info__content__info > p.text-h1.highlighted::text").get()
+        datum_registrace = response.css('div.detail-hero__info__boxes > div.detail-hero__info__box:nth-of-type(1)::text').getall()
+        najezd = response.css('div.detail-hero__info__boxes > div.detail-hero__info__box:nth-of-type(2)::text').getall()
+        stk = response.xpath("//div[@class='detail-box-M']/div/div[3]/text()").getall()
+        stav = response.xpath("//span[@class='detail-info__row__state__text']/text()").get().strip()
+        vykon = response.xpath('//div[@class="detail-info__col"][1]/div[2]/div[1]/div[1]/text()').getall()
+        objem_motoru = response.xpath('//div[@class="detail-info__col"][1]/div[2]/div[1]/div[2]/text()').getall()
+        typ_paliva = response.xpath('//div[@class="detail-info__col"][1]/div[2]/div[1]/div[3]/text()').getall()
+        typ_prevodovky = response.xpath('//div[@class="detail-info__col"][1]/div[2]/div[1]/div[4]/text()').getall()
+        typ_karoserie = response.xpath('//div[@class="detail-info__col"][2]/div[2]/div[1]/div[1]/text()').getall()
+        pocet_dveri = response.xpath('//div[@class="detail-info__col"][2]/div[2]/div[1]/div[2]/text()').getall()
+        pocet_mist = response.xpath('//div[@class="detail-info__col"][2]/div[2]/div[1]/div[3]/text()').getall()
+        barva_exterieru = response.xpath('//div[@class="detail-info__col"][2]/div[2]/div[1]/div[4]/text()').getall()
+
 
         # Čištění získaných řetězců
-
+        model_auta = re.sub(r'\s+', ' ', model_auta).strip()
+        verze_modelu = re.sub(r'\s+', ' ', verze_modelu).strip()
+        #Cena zůstává beze změny
+        datum_registrace = ' '.join(datum_registrace).strip().replace(' ', '')
+        najezd = ' '.join(najezd).strip().replace(' ', '')
+        stk = re.sub(r"\s+", " ", " ".join(stk).strip())
+        #Stav zůstává beze změny
+        vykon = re.sub(r"\s+", " ", " ".join(vykon).strip())
+        objem_motoru = re.sub(r"\s+", " ", " ".join(objem_motoru).strip())
+        typ_paliva = re.sub(r"\s+", " ", " ".join(typ_paliva).strip())
+        typ_prevodovky = re.sub(r"\s+", " ", " ".join(typ_prevodovky).strip())
+        typ_karoserie = re.sub(r"\s+", " ", " ".join(typ_karoserie).strip())
+        pocet_dveri = re.sub(r"\s+", " ", " ".join(pocet_dveri).strip())
+        pocet_mist = re.sub(r"\s+", " ", " ".join(pocet_mist).strip())
+        barva_exterieru = re.sub(r"\s+", " ", " ".join(barva_exterieru).strip())
 
         # Uložení dat do csv souboru
         with open("data/cars.csv", "a", newline="") as f:
             writer = csv.writer(f)
-            writer.writerow([car_model, model_version, price, registration_date, mileage, stk, vykon, objem_motoru, typ_paliva, typ_prevodovky, typ_karoserie, pocet_dveri, pocet_mist, barva_exterieru])
-            #writer.writerow([car_model, model_version, price, fuel_type, mileage, location, response.url])
+            writer.writerow([model_auta, verze_modelu, cena, datum_registrace, najezd, stk, stav, vykon,
+                             objem_motoru, typ_paliva, typ_prevodovky, typ_karoserie, pocet_dveri, pocet_mist,
+                             barva_exterieru])
