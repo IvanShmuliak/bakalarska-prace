5216c531ab92be6ab2c8586c19ac02edf030b30d
ivanshmuliak <shmi03@vse.cz>
2025-02-06 19:25:04 +0100

    Byl proveden kompletní sběr dat. Začátek 2025-02-06 v 17:51. Konec 2025-02-06 v 19:21. Celkem url 4120, celkem záznamů 4090.

--- src/_main_.py
+++ src/_main_.py
@@ -7,7 +7,7 @@ from src.spiders.car_details import CarDetailsSpider
 # Nastavení logování
 configure_logging()
 runner = CrawlerRunner(settings={
-        "ROBOTSTXT_OBEY": True,  # Respektování robots.txt
+        "ROBOTSTXT_OBEY": True,  # Respektování souboru robots.txt
         "DOWNLOAD_DELAY": 1,     # Prodleva mezi požadavky
         "CONCURRENT_REQUESTS": 2,  # Omezení paralelních požadavků
         "DEFAULT_REQUEST_HEADERS": {  # Nastavení HTTP hlaviček
@@ -16,9 +16,9 @@ runner = CrawlerRunner(settings={
 
 @defer.inlineCallbacks
 def run_spiders():
-    # Spustí prvního spidera
-    #yield runner.crawl(TipcarsSpider)
-    # Spustí druhého spidera po dokončení prvního
+    # Spuštění prvního spidera
+    yield runner.crawl(TipcarsSpider)
+    # Spuštění druhého spidera jen po dokončení prvního
     yield runner.crawl(CarDetailsSpider)
     # Ukončí reactor po dokončení druhého spidera
     reactor.stop()
